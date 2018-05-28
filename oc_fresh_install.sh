f8cf7d471a2a0d69e2be4b1a9f3a22ab894afa1c
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2018-05-28 10:04:15 +0530

    Update oc_fresh_install.sh

--- oc_fresh_install.sh
+++ oc_fresh_install.sh
@@ -46,6 +46,7 @@ echo "Nginx server installation completed"
 sleep 2;
 cd ~
 sudo chown www-data:www-data -R /var/www/"$DOMAIN"
+sudo chown www-data:www-data -R /var/www
 sudo systemctl restart nginx.service
 
 echo "lets install php 7.0 and modules"
